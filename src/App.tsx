84183375c641709b26315b4d82e31ccefde17d11
caseyjramirez <caseyjramirez5@gmail.com>
2024-08-23 17:12:00 -0500

    My first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Image from "./types/Image";
 
 function App() {
+  const [images, setImages] = useState<Image[]>([]);
+
+  useEffect(() => {
+    fetch('/images.json')
+      .then(response => response.json())
+      .then(data => setImages(data));
+  }, []);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="display-flex flex-direction-column ai-center mb-20">
+          <h1 className="">brb...</h1>
+          <h3 className="">website under constuction</h3>
+        </div>
+
+        <div className="image-grid mb-20">
+          {images.map((image, index) => (
+            <div className="image">
+              <img src={image.filePath} alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/types/Image.ts
@@ -0,0 +1,10 @@
+export default interface Image {
+    id: string;
+    filePath: string;
+    name: string;
+    city: string;
+    date: string;
+    description: string;
+    imageType: string;
+    aspectRatio: string[];
+}
