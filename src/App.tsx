4d0cbbc0d8a55015f6ce581f5acdf4af6c5852c6
caseyjramirez <caseyjramirez5@gmail.com>
2024-08-26 08:32:49 -0500

    - updated the home page. - added a github actions file to manage deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
-import { useEffect, useState } from "react";
-import Image from "./types/Image";
+import constructionDoge from "./assets/construction_doge.jpg";
 
 function App() {
-  const [images, setImages] = useState<Image[]>([]);
-
-  useEffect(() => {
-    fetch('/images.json')
-      .then(response => response.json())
-      .then(data => setImages(data));
-  }, []);
 
   
   return (
     <div className="App">
       <div className="container">
-        <div className="display-flex flex-direction-column ai-center mb-20">
+        <div className="home">
           <h1 className="">brb...</h1>
-          <h3 className="">website under constuction</h3>
-        </div>
+          <h3 className="mb-20">under construction</h3>
+          <div className="">
+            <img src={constructionDoge} alt="construction doge" />
+          </div>
 
-        <div className="image-grid mb-20">
-          {images.map((image, index) => (
-            <div className="image">
-              <img src={image.filePath} alt="" />
-            </div>
-          ))}
         </div>
       </div>
     </div>
